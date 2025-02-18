a19ffd59ae1119f587103625422b3901fbc826d4
hastmark <hastmark2001@hotmail.com>
2025-02-18 09:27:44 +0100

    Updated sample

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,29 +1,15 @@
 
-#include "smath/core.h"
-#include <iostream>
+#include "smath/matrix.h"
+#include "sycl/sycl.hpp"
+#include <cstddef>
+
+constexpr size_t M_DIM{64};
+constexpr size_t N_DIM{64};
 
 int main() {
-  smath::Matrix<float, 3, 2> a_mat;
-  smath::Matrix<float, 2, 4> b_mat;
-
-  for (auto &row : a_mat.raw()) {
-    for (auto &elem : row) {
-      elem = 1.0f;
-    }
-  }
-
-  for (auto &row : b_mat.raw()) {
-    for (auto &elem : row) {
-      elem = 2.0f;
-    }
-  }
-
+  auto queue = sycl::queue{};
+  smath::Matrix<float, M_DIM, N_DIM> a_mat{&queue};
+  smath::Matrix<float, M_DIM, N_DIM> b_mat{&queue};
   auto c_mat = a_mat * b_mat;
-
-  for (const auto &row : c_mat.craw()) {
-    for (const auto &elem : row) {
-      std::cout << elem << "\n";
-    }
-  }
   return 0;
 }
